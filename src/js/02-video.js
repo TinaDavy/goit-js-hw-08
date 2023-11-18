f80289e905fd4e4b06a55d220853ded266708625
taniadavy <tania111davidenko111@gmail.com>
2023-11-18 23:29:39 +0100

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+console.log(player);
+
+const onPlay = _.throttle(function(data) {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+    }).catch(function(error) {
+        console.log(error.message);
+    });
+},1000);
+
+player.on('timeupdate', onPlay);
+
+const seconds = Number(localStorage.getItem("videoplayer-current-time"));
+
+player.setCurrentTime(seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+}); 
 
